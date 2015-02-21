c6c557d613f263a8dafedeef2ef1fb427db6d1e8
ka <ka.kaosf@gmail.com>
2015-02-21 15:33:04 +0900

    [WIP]

--- test/tapl4/core_test.clj
+++ test/tapl4/core_test.clj
@@ -15,14 +15,22 @@
          (:pred :zero) :zero))
   (testing ":pred"
     (are [x y] (= (myeval '(:pred x)) y)
-         :zero        '(:pred :zero)
+         :zero         '(:pred :zero)
          (:succ :zero) :zero))
+  (testing "nested :succ"
+    (are [x y] (= (myeval x) y)
+         (myeval '(:succ :zero)) '(:succ :zero)
+         (myeval '(:succ (:succ :zero))) '(:succ (:succ :zero))))
+  (testing "nested :pred"
+    (are [x y] (= (myeval x) y)
+         (myeval '(:pred :zero)) '(:pred :zero)
+         (myeval '(:pred (:pred :zero))) '(:pred (:pred :zero))))
   (testing ":iszero"
     (are [x y] (= (myeval '(:iszero x)) y)
-         :zero          :true
+         :zero         :true
          (:succ :zero) :false
          (:pred :zero) :false))
   (testing ":ifthenelse"
-    (are [c t e  x] (= (myeval '(:ifthenelse c t e)) x)
-         :true :zero :false  :zero
+    (are [c t e x] (= (myeval '(:ifthenelse c t e)) x)
+         :true  :zero :false :zero
          :false :true :zero  :zero)))
