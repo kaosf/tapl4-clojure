5861e14bb0d63813f7607b31140ed79bc16ef2ed
ka <ka.kaosf@gmail.com>
2015-03-21 16:33:37 +0900

    Implement numericval? function

--- src/tapl4/core.clj
+++ src/tapl4/core.clj
@@ -1,2 +1,9 @@
 (ns tapl4.core
   (:gen-class))
+
+(defn numericval? [t]
+  (case (first t)
+    :zero true
+    :succ (numericval? (second t))
+    :pred (numericval? (second t))
+    false))
--- test/tapl4/core_test.clj
+++ test/tapl4/core_test.clj
@@ -1,3 +1,13 @@
 (ns tapl4.core-test
   (:require [clojure.test :refer :all]
             [tapl4.core :refer :all]))
+
+(deftest myeval-test
+  (testing "numericval?"
+    (is (= true  (numericval? '(:zero))))
+    (is (= true  (numericval? '(:succ (:zero)))))
+    (is (= true  (numericval? '(:pred (:zero)))))
+    (is (= false (numericval? '(:true))))
+    (is (= false (numericval? '(:false))))
+    )
+  )
