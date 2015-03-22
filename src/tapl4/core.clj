75341cf9400f942fa3112e9e3bb6ff60a436cdb8
ka <ka.kaosf@gmail.com>
2015-03-23 00:09:27 +0900

    [WIP] Implement eval1 function (if, succ, pred)

--- src/tapl4/core.clj
+++ src/tapl4/core.clj
@@ -14,3 +14,21 @@
     :true  true
     :false true
     (numericval? t)))
+
+(defn eval1 [t]
+  (case (first t)
+    :if (let [[t1 t2 t3] (rest t)]
+          (case (first t1)
+            :true t2
+            :false t3
+            `(:if ~(eval1 t1) ~t2 ~t3)))
+    :succ (let [[t1] (rest t)]
+            `(:succ ~(eval1 t1)))
+    :pred (let [[t1] (rest t)]
+               (case (first t1)
+                 :zero '(:zero)
+                 :succ (when-let [nv1 (second t1)] (numericval? nv1) nv1)
+                 `(:pred ~(eval1 t1))))
+    t
+    )
+  )
