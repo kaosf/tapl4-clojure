44f1f3c8ced481a8b2bb9fb4b676fd34cd12541b
ka <ka.kaosf@gmail.com>
2015-03-26 00:11:51 +0900

    [WIP]

--- src/tapl4/core.clj
+++ src/tapl4/core.clj
@@ -27,7 +27,10 @@
     :pred (let [[t1] (rest t)]
                (case (first t1)
                  :zero '(:zero)
-                 :succ (when-let [nv1 (second t1)] (numericval? nv1) nv1)
+                 :succ (let [nv1 (second t1)]
+                         (if (numericval? nv1)
+                           nv1
+                           t))
                  `(:pred ~(eval1 t1))))
     :iszero (let [[t1] (rest t)]
               (case (first t1)
